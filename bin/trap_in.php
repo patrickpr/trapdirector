a52a9598dab920428944d8e1890db8a796af8667
patrickpr <patrick@proy.org>
2019-01-13 23:47:49 +0100

    cleanup trap_in

--- bin/trap_in.php
+++ bin/trap_in.php
@@ -41,25 +41,4 @@ $Trap->add_rule_final(microtime(true) - $time1);
 
 exit(0);
 
-/*
-Check services in down state : 
-	 icingacli monitoring list services --columns 'host,service,service_state,service_last_state_change' --format='"$host$" "$service$" $service_state$ $service_last_state_change$'
-	  --problem : hard et not ack.	 
-	 ou format=csv 
-            'host_name',
-            'host_state',
-            'host_output',
-            'host_handled',
-            'host_acknowledged',
-            'host_in_downtime',
-            'service_description',
-            'service_state',
-            'service_acknowledged',
-            'service_in_downtime',
-            'service_handled',
-            'service_output',
-            'service_perfdata',
-            'service_last_state_change'
-	 
-*/	 
 ?>
