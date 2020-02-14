7048d43d3553bb028462b2d7b8b7baab4f28cdda
patrickpr <>
2020-02-14 15:11:35 +0100

    order of inclusion

--- bin/trap_class.php
+++ bin/trap_class.php
@@ -6,8 +6,10 @@
 include (dirname(__DIR__).'/library/Trapdirector/Icinga2Api.php');
 include (dirname(__DIR__).'/library/Trapdirector/TrapsProcess/Logging.php');
 include (dirname(__DIR__).'/library/Trapdirector/TrapsProcess/Database.php');
-include (dirname(__DIR__).'/library/Trapdirector/TrapsProcess/Mib.php');
+
 include (dirname(__DIR__).'/library/Trapdirector/TrapsProcess/MibDatabase.php');
+include (dirname(__DIR__).'/library/Trapdirector/TrapsProcess/Mib.php');
+
 
 include (dirname(__DIR__).'/library/Trapdirector/TrapsProcess/Rule.php');
 include (dirname(__DIR__).'/library/Trapdirector/TrapsProcess/Plugins.php');
