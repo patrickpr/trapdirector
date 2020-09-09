96ea3bb889121e398bc74741723f201a65844863
patrickpr <>
2020-09-09 17:58:45 +0200

    Starting js push to module.js

--- public/js/module.js
+++ public/js/module.js
@@ -1,21 +1,75 @@
 
-// 
+// **************** Message display
 
-// **************** Helpers
+(function (Icinga) {
 
-// Handler helpers address
-hostHelper=webPath+'helper/gethosts';
-hostgroupHelper=webPath+'helper/gethostgroups';
-serviceHelper=webPath+'helper/getservices';
-hostgroupserviceHelper=webPath+'helper/gethostgroupservices';
-trapfrommmibHelper=webPath+'helper/gettraps';
-translateoidHelper=webPath+'helper/translateoid';
-getobjectsHelper=webPath+'helper/gettrapobjects';
-form_submit=webPath+'handler/handlerform';
-testHelper=webPath+'helper/testrule';
+    var TrapDirector= function (module) {
+        this.module = module;
+
+        this.initialize();
+
+        this.module.icinga.logger.debug('Trapdirector module loaded');
+    };
+
+    TrapDirector.prototype = {
+
+        initialize: function () {
+        },
+
+        displayWarning: function(message) {
+            $("#footer > #notifications").html('<li class="error">'+message+'</li>');
+            $('#notifications li').addClass('fading-out').delay(6000).fadeOut('slow',function() { $(this).remove(); });
+	            
+        },
+        
+        displayOK : function(message)
+        {
+            $("#footer > #notifications").html('<li class="success">'+message+'</li>');
+            $('#notifications li').addClass('fading-out').delay(3000).fadeOut('slow',function() { $(this).remove(); });
+        },
+        
+        ajaxCall (url, input, success, error_returned , error_server)
+		{
+			success = (success !== undefined && success !== null) ? 
+				success 
+				: function(data) { TrapDirector.prototype.displayOK('Done'); } ;
+			
+			error_returned = (error_returned !== undefined && error_returned !== null) ? 
+				error_returned
+				: function(data) { TrapDirector.prototype.displayWarning("Error : "+ data.status); };
+			error_server  = (error_server !== undefined && error_server !== null) ? 
+				error_server : error_returned;
+
+			$.ajax({
+					url: url,
+					async: true,
+					dataType: "json",
+					type: "POST",
+					data: input,
+			        success: function (data) 
+					{
+						if (data.status == "OK") 
+						{
+							success(data);
+						} 
+						else 
+						{
+							error_returned(data);		
+						}
+					},
+					error: function (data)
+					{
+						error_returned(data);
+					}
+			});						
+			
+		}
+
+              
+    };
+
+    Icinga.availableModules.trapdirector = TrapDirector;
+
+}(Icinga));
 
 
-function test1() {
-
-	
-}
